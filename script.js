50dfe7a4e3a84385973713314ad85b46ff828c52
James Hunter <jamessahunter@gmail.com>
2023-09-26 11:16:52 -0700

    add prompt and confirm

--- script.js
+++ script.js
@@ -1,8 +1,23 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+
+function getCriteria(){
+  console.log("test")
+  var length=prompt("PLease enter the length of password you would like to generate.");
+  while(length<8 || length>128){
+    length=prompt("Please enter a number between 8 and 128.");
+  }
+  var lowercase=confirm("Please confirm if you would like to use lowercase characters in your password.");
+  var uppercase=confirm("Please confirm if you would like to use uppercase characters in your password.");
+  var numbers=confirm("Please confirm if you would like to use numeric characters in your password.");
+  var special=confirm("Please confirm if you would like to use special characters in your password.");
+  return;
+}
+
 // Write password to the #password input
 function writePassword() {
+  getCriteria();
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
