d4c6b83b1e8c6d6a6c80c4f028a4b8ba8e52e6f2
James Hunter <jamessahunter@gmail.com>
2023-09-26 13:16:17 -0700

    add initial conditions

--- script.js
+++ script.js
@@ -1,24 +1,59 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var alphabetUpperArr=["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+var alphabetLowerArr=["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+var numArr=["0","1","2","3","4","5","6","7","8","9"];
+var specialArr=["\u0022","\u0027","\u005C"," ","!","#","$","%","&","(",",",")","*","+","-",".","/",":",";","<","=",">","?","@","[","]","^","_","`","{","|","}","~"];
 
+
+var  criteriaArr=[];
+var Arr=[];
 
 function getCriteria(){
-  console.log("test")
-  var length=prompt("PLease enter the length of password you would like to generate.");
-  while(length<8 || length>128){
+  // console.log("test")
+  var passLength=prompt("PLease enter the length of password you would like to generate.");
+  while(passLength<8 || passLength>128){
     length=prompt("Please enter a number between 8 and 128.");
   }
   var lowercase=confirm("Please confirm if you would like to use lowercase characters in your password.");
   var uppercase=confirm("Please confirm if you would like to use uppercase characters in your password.");
   var numbers=confirm("Please confirm if you would like to use numeric characters in your password.");
   var special=confirm("Please confirm if you would like to use special characters in your password.");
+  return [passLength,lowercase,uppercase,numbers,special];
+}
+
+function generatePassword(Arr){
+  console.log(Arr);
+  if(Arr[1]){//lowercase
+    console.log("lower");
+  }
+  else{
+
+  }
+  if(Arr[2]){//uppercase
+    console.log("upper");
+  }
+  else{
+
+  }
+  if(Arr[3]){//numbers
+    console.log("number");
+  }
+  else{
+
+  }
+  if(Arr[4]){//special
+    console.log("special");
+  }
   return;
 }
 
+// console.log(specialArr[0],specialArr[1],specialArr[2]);
 // Write password to the #password input
 function writePassword() {
-  getCriteria();
-  var password = generatePassword();
+  criteriaArr=getCriteria();
+  // console.log(criteriaArr);
+  var password = generatePassword(criteriaArr);
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
